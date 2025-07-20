86ad90f9d1dbfc1240d46f99d167b0ca1831243a
nehaapu <samiyanurriya38@gmail.com>
2025-07-20 23:43:42 +0600

    Update 0admin.js

--- Script/commands/0admin.js
+++ Script/commands/0admin.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "0admin",
 	version: "1.0.5",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "neha ammu",
 	description: "Admin Config",
 	commandCategory: "Admin",
 	usages: "Admin",
